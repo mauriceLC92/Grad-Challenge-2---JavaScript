cb10b6367683fce441759731405d6a60407d0439
Maurice Le Cordier <maurice.lecordier@SAMAC141.local>
2018-01-30 12:43:43 +0200

    Added ch4 file

---
+++ Challenge4.js
@@ -0,0 +1,13 @@
+/*
+Challenge 4 - Indexing & Searching
+
+We have a large array of users which are comprised of 3 different user types ("Admin", "Moderator", "User").
+Each user will have the following properties: ("name", "companyId", "type").
+
+Your first method will take 1 parameter: an array of users and must return a grouped list according to the user property "type".
+
+The second method will take 4 parameters: a grouped list (result from the first method), an array of user types to include in the search,
+a string representing the user property to filter by and a string representing the value of the user property. 
+This method must return an array of users as per the search parameters.
+
+*/
