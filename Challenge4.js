8f35d13496330f9880322208a876b372c0c6d544
Maurice Le Cordier <maurice.lecordier@SAMAC141.local>
2018-01-30 16:26:49 +0200

    added method one for ch4

--- Challenge4.js
+++ Challenge4.js
@@ -1,3 +1,4 @@
+
 /*
 Challenge 4 - Indexing & Searching
 
@@ -11,3 +12,72 @@ a string representing the user property to filter by and a string representing t
 This method must return an array of users as per the search parameters.
 
 */
+
+let userList = [
+    {
+      "name": "Joe",
+      "companyId": "A2100",
+      "type": "Admin"
+    },
+    {
+      "name": "Jane",
+      "companyId": "A2100",
+      "type": "Moderator"
+    },
+    {
+      "name": "Smith",
+      "companyId": "A2100",
+      "type": "User"
+    },
+    {
+      "name": "Smith",
+      "companyId": "A2100",
+      "type": "User"
+    },
+    {
+      "name": "Rocket",
+      "companyId": "A3100",
+      "type": "Admin"
+    },
+    {
+      "name": "Rick",
+      "companyId": "A3100",
+      "type": "User"
+    },
+    {
+      "name": "Tim",
+      "companyId": "A4100",
+      "type": "Admin"
+    }
+  ];
+
+let result = [];
+const Admin = {
+    name: "",
+    companyId: "",
+    type: "",
+};
+
+const Moderator = {
+    name: "",
+    companyId: "",
+    type: "",
+};
+
+const User = {
+    name: "",
+    companyId: "",
+    type: "",
+};
+
+function orchestrateUsers(users) // Method 1
+{
+    result = users.reduce(function (r, a) {
+        r[a.type] = r[a.type] || [];
+        r[a.type].push(a);
+        return r;
+    }, Object.create(null));
+}
+
+orchestrateUsers(userList);
+console.log(result);
