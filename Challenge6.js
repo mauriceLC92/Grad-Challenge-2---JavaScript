7f3d784891fc4f6422f789ac37b8fed0cda076b7
Maurice Le Cordier <maurice.lecordier@SAMAC141.local>
2018-01-31 15:34:56 +0200

    added ch6 solution

--- Challenge6.js
+++ Challenge6.js
@@ -4,3 +4,21 @@ Challenge 6 - Recursion
 Create a function that makes use of recursion to add the all the "total" properties up and return back to the user
 */
 
+let fs = require('fs');
+let recursiveData = JSON.parse(fs.readFileSync('recursionData.json', 'utf8'));
+
+
+function totalSum(obj)
+{
+    var total = 0;
+    for(var x in obj)
+    {
+        if(typeof obj[x] === "number")
+            total += obj[x];
+        else
+            total += totalSum(obj[x]);
+    }
+    return total;
+}
+
+console.log(totalSum(recursiveData));
